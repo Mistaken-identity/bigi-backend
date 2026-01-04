c70a5f2072afd08b15cbe30f0730a6e73d9bc9ca
mistaken-identity <antonyteflon10@gmail.com>
2026-01-04 09:02:01 +0300

    Add health check route

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ app.use('/auth', authRoutes);
 app.use('/orders', orderRoutes);
 
 const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => { 
+    res.send('Bigi backend is running 🚀'); 
+});
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
