a870fbcdfd17abfbf311ec7456e45ac25586c9c2
mistaken-identity <antonyteflon10@gmail.com>
2026-01-04 08:41:55 +0300

    Updated for redeployment

--- server.js
+++ server.js
@@ -14,13 +14,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log(' MongoDB connected'))
 .catch(err => console.error('Could not connect to MongoDB', err));
 
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/orders', require('./routes/Orders'));
+app.use('/auth', authRoutes);
+app.use('/orders', orderRoutes);
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server running on port ${process.env.PORT || 5000}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
