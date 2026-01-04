940973a46d34c2fb74a0b7e24b9aa64d5cf454fd
mistaken-identity <antonyteflon10@gmail.com>
2026-01-04 10:47:41 +0300

    Fix ES module imports and Render crash

--- server.js
+++ server.js
@@ -3,28 +3,29 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import authRoutes from "./routes/auth.js";
+import orderRoutes from "./routes/Orders.js";
+
 dotenv.config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+.connect(process.env.MONGO_URI)
 .then(() => console.log(' MongoDB connected'))
 .catch(err => console.error('Could not connect to MongoDB', err));
+
+app.get('/', (req, res) => { 
+    res.send('Hello Teflon, Bigi backend is running 🚀'); 
+});
 
 app.use('/auth', authRoutes);
 app.use('/orders', orderRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.get('/', (req, res) => { 
-    res.send('Bigi backend is running 🚀'); 
-});
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
